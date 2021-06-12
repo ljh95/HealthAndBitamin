16c9f9270417efed8abf4f6fbd9eab1b95b6237d
ilegwmam <ilegwmam@gmail.com>
2021-06-12 15:01:47 +0900

    ♻️ Success to hook

--- src/Pages/Success/Success.js
+++ src/Pages/Success/Success.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Success.scss';
 
-class Success extends Component {
-  render() {
-    return (
-      <div className="success">
-        <div className="messageBox">
-          <p className="desc">결제가 완료되었습니다!</p>
-          <p className="hi">
-            Have a Vitamin Day<i class="fas fa-heart"></i>
-          </p>
+function Success() {
+  const history = useHistory();
+  return (
+    <div className="success">
+      <div className="messageBox">
+        <p className="desc">결제가 완료되었습니다!</p>
+        <p className="hi">
+          Have a Vitamin Day<i class="fas fa-heart"></i>
+        </p>
 
-          <div>
-            <button
-              className="btn"
-              onClick={() => {
-                this.props.history.push('/');
-              }}
-            >
-              Go Main
-            </button>
-          </div>
+        <div>
+          <button
+            className="btn"
+            onClick={() => {
+              history.push('/');
+            }}
+          >
+            Go Main
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withRouter(Success);
+export default Success;
