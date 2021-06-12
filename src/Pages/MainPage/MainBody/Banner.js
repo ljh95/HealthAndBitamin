c376789a96ac55d7976f8cd18c5b22c45cb9c1ff
ilegwmam <ilegwmam@gmail.com>
2021-06-12 13:32:41 +0900

    ♻️ Main to Hook

--- src/Pages/MainPage/MainBody/Banner.js
+++ src/Pages/MainPage/MainBody/Banner.js
@@ -1,38 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Banner.scss';
-
-export default class Banner extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentBannerIndex: 0,
-    };
-  }
-
-  changeImage = () => {
-    const { currentBannerIndex } = this.state;
-
-    this.setState({
-      currentBannerIndex: currentBannerIndex ? 0 : 1,
-    });
-  };
-
-  render() {
-    const { currentBannerIndex } = this.state;
-
-    return (
-      <section className="bannerSection" onClick={this.changeImage}>
-        <img
-          src={bannerList[currentBannerIndex]}
-          alt="benner"
-          className="bennerImage"
-        />
-      </section>
-    );
-  }
-}
 
 const bannerList = [
   'https://cdn.pixabay.com/photo/2016/07/24/21/01/thermometer-1539191_960_720.jpg',
   'https://cdn.pixabay.com/photo/2016/06/26/23/47/poison-1481596_960_720.jpg',
 ];
+
+export default function Banner() {
+  const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
+
+  const changeImage = () => {
+    setCurrentBannerIndex(currentBannerIndex ? 0 : 1);
+  };
+
+  return (
+    <section className="bannerSection" onClick={changeImage}>
+      <img
+        src={bannerList[currentBannerIndex]}
+        alt="benner"
+        className="bennerImage"
+      />
+    </section>
+  );
+}
--- src/Pages/MainPage/MainBody/Best.js
+++ src/Pages/MainPage/MainBody/Best.js
@@ -1,69 +1,60 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Best.scss';
 
-class Best extends Component {
-  constructor() {
-    super();
-    this.state = { bestList: [] };
-  }
+function Best() {
+  const history = useHistory();
+  const [bestList, setBestList] = useState([]);
 
-  componentDidMount() {
-    // fetch('http://localhost:8000/products/main-best')
+  useEffect(() => {
     fetch('http://18.116.64.187:8000/products/main-best')
       .then(res => res.json())
       .then(data => {
-        this.setState({
-          bestList: data.BEST_PRODUCT.slice(0, 8),
-        });
+        setBestList(data.BEST_PRODUCT.slice(0, 8));
       });
-  }
+  }, []);
 
-  makeTotalPrice = (price, discount) => {
+  const makeTotalPrice = (price, discount) => {
     return (price - (price * discount) / 100).toLocaleString();
   };
 
-  goDetailPage = id => {
-    this.props.history.push(`/productInfo/${id}`);
+  const goDetailPage = id => {
+    history.push(`/productInfo/${id}`);
   };
 
-  render() {
-    const { bestList } = this.state;
+  return (
+    <section className="bestSection">
+      <header>
+        <h1>BEST</h1>
+        <p>지금 가장 잘나가는 상품을 만나보세요</p>
+      </header>
+      <ul className="productList">
+        {bestList.map(product => {
+          const { product_id, image, name, price, discount } = product;
 
-    return (
-      <section className="bestSection">
-        <header>
-          <h1>BEST</h1>
-          <p>지금 가장 잘나가는 상품을 만나보세요</p>
-        </header>
-        <ul className="productList">
-          {bestList.map(product => {
-            const { product_id, image, name, price, discount } = product;
-
-            return (
-              <article
-                key={product_id}
-                className="product"
-                onClick={() => this.goDetailPage(product_id)}
-              >
-                <img className="productImage" src={image} alt="product" />
-                <p className="name">
-                  {name} <i className="fas fa-search" />
-                </p>
-                <p className="price">{price.toLocaleString()}원</p>
-                <p className="result">
-                  <span className="applyPrice">
-                    {this.makeTotalPrice(price, discount)}원
-                  </span>
-                  <span className="discount">({discount}%)할인</span>
-                </p>
-              </article>
-            );
-          })}
-        </ul>
-      </section>
-    );
-  }
+          return (
+            <article
+              key={product_id}
+              className="product"
+              onClick={() => goDetailPage(product_id)}
+            >
+              <img className="productImage" src={image} alt="product" />
+              <p className="name">
+                {name} <i className="fas fa-search" />
+              </p>
+              <p className="price">{price.toLocaleString()}원</p>
+              <p className="result">
+                <span className="applyPrice">
+                  {makeTotalPrice(price, discount)}원
+                </span>
+                <span className="discount">({discount}%)할인</span>
+              </p>
+            </article>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
 
-export default withRouter(Best);
+export default Best;
